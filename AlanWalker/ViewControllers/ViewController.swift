0239f6552a84a12a2c01836a523a74ac354294b1
Hady Helal <helalhady16@gmail.com>
2020-05-21 22:08:18 +0200

    initial login UI

---
+++ AlanWalker/ViewControllers/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  AlanWalker
+//
+//  Created by Hady on 5/21/20.
+//  Copyright © 2020 HadyOrg. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    @IBAction func logInButton(_ sender: Any) {
+        performSegue(withIdentifier: "loginSegue", sender: self)
+    }
+    
+    @IBAction func registerButton(_ sender: Any) {
+        performSegue(withIdentifier: "RegisterSegue", sender: self)
+    }
+    
+}
+
